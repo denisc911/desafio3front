4c7bfb66bfebc9ceb35d15dd6e71bf7336d79cd4
victudor09 <victudor09@gmail.com>
2024-10-09 12:51:28 +0200

    Base del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // App.js
 
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SavingsFormStep1 from './components/SavingsFormStep1';
+import SavingsFormStep2 from './components/SavingsFormStep2';
 
 function App() {
-  const [language, setLanguage] = useState('EN');
- 
-  const handleLanguageChange = (newLanguage) => {
-    setLanguage(newLanguage); // Update the language state
-  };
-  return (
-    <div>
-      <Header language={language} onLanguageChange={handleLanguageChange} />
-      <Home />
-      <Footer />
-    </div>
-  );
+	const [language, setLanguage] = useState('EN');
+
+	const handleLanguageChange = (newLanguage) => {
+		setLanguage(newLanguage); // Update the language state
+	};
+	return (
+		<BrowserRouter>
+			<Header language={language} onLanguageChange={handleLanguageChange} />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/step1" element={<SavingsFormStep1 />} />
+				<Route path="/step2/:type" element={<SavingsFormStep2 />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 // components/Home.js
+import Form from '../components/Form'
+import SavingsFormStep1 from './SavingsFormStep1';
 
 const Home = () => {
   return (
     <section className="bg-light text-dark p-5">
       <div className="container">
         <h2>Welcome to Kutxabank</h2>
-        
-        <p></p>
+
+        <p>
+          <SavingsFormStep1 />
+        </p>
        
       </div>
     </section>
