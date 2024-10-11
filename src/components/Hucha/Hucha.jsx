d76cc2dc362944b88ff4b018ca8f35d566b643e3
kepapujana <kepapujana20@gmail.com>
2024-10-11 19:30:34 +0200

    Hucha

---
+++ src/components/Hucha/Hucha.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hucha = () => {
+  return (
+    <>
+      <form action="/action_page.php">
+        <input type="range" />
+      </form>
+    </>
+  );
+};
+
+export default Hucha;
