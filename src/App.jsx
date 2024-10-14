bfece1bc9f9d75aa36ea4ef1ecaa929f513f8b16
kepapujana <kepapujana20@gmail.com>
2024-10-14 18:39:40 +0200

    hucha changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import ChartsCarousel from './components/charts/ChartsCarousel';
-import Hucha from './components/Hucha/Hucha';
+import Hucha from './components/hucha/Hucha';
 
 function App() {
   const [language, setLanguage] = useState('EN');
@@ -20,8 +20,8 @@ function App() {
 
       <div className="container mt-5">
         <Routes>
-          {/* <Route path="/" element={<Home />} />  */}
-          {/* <Route path="/chart" element={<ChartsCarousel />} />  */}
+          <Route path="/" element={<Home />} />
+          <Route path="/chart" element={<ChartsCarousel />} />
           <Route path="/hucha" element={<Hucha />} />
         </Routes>
       </div>
