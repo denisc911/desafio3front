22028e68f7973ca93749c61c92af84243d055351
chuans <juaazn.05@gmail.com>
2024-10-16 23:47:24 +0200

    picture tag added to image

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,31 +1,35 @@
 import '../../style/chart/FooterMenu.css';
 import { Link } from 'react-router-dom';
 
-const FooterMenu = () => {
+export default function Nav () {
 	return (
 		<nav className="footer-menu">
 			<Link to="/">
 				<div className="menu-item">
-					<i className="fas fa-home"></i>
+					<picture>
+						<i className="fas fa-home"></i>
+					</picture>
 					<p>Home</p>
 				</div>
 			</Link>
 
 			<Link to='/hucha'>
 				<div className="menu-item">
-					<i className="fa-solid fa-piggy-bank"></i>
+					<picture>
+						<i className="fa-solid fa-piggy-bank"></i>
+					</picture>
 					<p>Hucha</p>
 				</div>
 			</Link>
 
 			<Link to='/profile'>
 				<div className="menu-item">
-					<i className="fas fa-user"></i>
+					<picture>
+						<i className="fas fa-user"></i>
+					</picture>
 					<p>Profile</p>
 				</div>
 			</Link>
 		</nav>
 	);
 };
-
-export default FooterMenu;
