faff89335611e4f93ba2b5ca6d176388b9615fc0
denisc911 <denis.cabrera1990@gmail.com>
2024-10-18 16:20:36 +0200

    cambios commitados previo pulleo

--- src/components/SavingsFormStep1.jsx
+++ src/components/SavingsFormStep1.jsx
@@ -2,36 +2,51 @@ import { Formik } from 'formik';
 import { useDispatch } from 'react-redux';
 import { setSavingsType } from '../redux/slices/savingsSlice';
 import { useNavigate } from 'react-router-dom';
-import '../style/SavingsFormStep1.css'; 
+import { OkFingerIcon, BulbChargingIcon, MoneySavingJarIcon, CustomizeIcon } from 'hugeicons-react'; // Importa los íconos necesarios
+import '../style/SavingsFormStep1.css';
 
 const SavingsFormStep1 = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const savingsOptions = [
-    { id: 'facil', label: 'Ahorro Fácil' },
-    { id: 'moderado', label: 'Ahorro Moderado' },
-    { id: 'extremo', label: 'Ahorro Extremo' },
-    { id: 'personalizado', label: 'Ahorro Personalizado' },
+    { id: 'facil', label: 'Fácil' },
+    { id: 'moderado', label: 'Inteligente' },
+    { id: 'extremo', label: 'Extremo' },
+    { id: 'personalizado', label: 'Personalizado' },
   ];
 
   const handleSelect = (option) => {
-    dispatch(setSavingsType(option)); // Сохраняем тип сбережений в Redux
-    navigate(`/step2/${option}`); 
+    dispatch(setSavingsType(option)); 
+    navigate(`/step2/${option}`);
   };
 
   return (
     <Formik initialValues={{ savingsType: '' }} onSubmit={() => {}}>
       {() => (
         <div className="savings-form-step1">
-          <h1>Elige qué tipo de ahorro te gustaría planificar</h1> {/* Заголовок не внутри <p> */}
+          <h1>Elige qué tipo de ahorro te gustaría planificar</h1>
           <div className="card-container">
             {savingsOptions.map((option) => (
               <div
                 key={option.id}
                 className="card"
                 onClick={() => handleSelect(option.id)}>
-                <h3>{option.label}</h3> {/* Заголовок для карточки */}
+                <h3>
+                  {option.label}
+                  {option.id === 'facil' && (
+                    <OkFingerIcon className="icon" style={{ marginLeft: '8px' }} />
+                  )}
+                  {option.id === 'moderado' && (
+                    <BulbChargingIcon className="icon" style={{ marginLeft: '8px' }} />
+                  )}
+                  {option.id === 'extremo' && (
+                    <MoneySavingJarIcon className="icon" style={{ marginLeft: '8px' }} />
+                  )}
+                  {option.id === 'personalizado' && (
+                    <CustomizeIcon className="icon" style={{ marginLeft: '8px' }} />
+                  )}
+                </h3>
               </div>
             ))}
           </div>
@@ -41,4 +56,5 @@ const SavingsFormStep1 = () => {
   );
 };
 
-export default SavingsFormStep1; // Экспортируем компонент
+export default SavingsFormStep1;
+git 
