493b7085e952dfa61309aba6aab211a7ab033c43
kepapujana <kepapujana20@gmail.com>
2024-10-22 18:12:47 +0200

    subscriptions components

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,20 +1,22 @@
 import style from '../../style/menu/Nav.module.css';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import subscriptions from '../../assets/subscribe-icon.svg';
 
 export default function Nav() {
-	const localUser = JSON.parse(localStorage.getItem('user')) || null; // Obtiene el usuario del localStorage
+  const { user } = useSelector((state) => state.auth);
+  const localUser = JSON.parse(localStorage.getItem('user')) || null; // Obtiene el usuario del localStorage
 
-	return (
-		<nav className={style.menu}>
-			<Link className={style.link} to="/">
-				<div className={style.menu_item}>
-					<picture>
-						<img src="/icon-home.svg" alt="home" />
-					</picture>
-					<p>Home</p>
-				</div>
-			</Link>
+  return (
+    <nav className={style.menu}>
+      <Link className={style.link} to="/">
+        <div className={style.menu_item}>
+          <picture>
+            <img src="/icon-home.svg" alt="home" />
+          </picture>
+          <p>Home</p>
+        </div>
+      </Link>
 
       <Link className={style.link} to="/hucha">
         <div className={style.menu_item}>
@@ -34,29 +36,29 @@ export default function Nav() {
         </div>
       </Link>
 
-			{user ? (
-				<>
-					<Link className={style.link} to="/profile">
-						<div className={style.menu_item}>
-							<picture>
-								<img src="/profile.svg" alt="piggy-bank" />
-							</picture>
-							<p>Profile</p>
-						</div>
-					</Link>
-				</>
-			) : (
-				<>
-					<Link className={style.link} to="/login">
-						<div className={style.menu_item}>
-							<picture>
-								<img src="/profile.svg" alt="piggy-bank" />
-							</picture>
-							<p>Profile</p>
-						</div>
-					</Link>
-				</>
-			)}
-		</nav>
-	);
+      {user ? (
+        <>
+          <Link className={style.link} to="/profile">
+            <div className={style.menu_item}>
+              <picture>
+                <img src="/profile.svg" alt="piggy-bank" />
+              </picture>
+              <p>Profile</p>
+            </div>
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link className={style.link} to="/login">
+            <div className={style.menu_item}>
+              <picture>
+                <img src="/profile.svg" alt="piggy-bank" />
+              </picture>
+              <p>Profile</p>
+            </div>
+          </Link>
+        </>
+      )}
+    </nav>
+  );
 }
